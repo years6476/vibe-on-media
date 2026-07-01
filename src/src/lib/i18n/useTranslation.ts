757122c0f32238f1a11fbe392a18a7a25c73f3a7
years6476 <amanulam94@gmail.com>
2026-07-01 15:47:45 +0600

    Delete src/src/lib/i18n directory

--- src/src/lib/i18n/useTranslation.ts
+++
@@ -1,5 +0,0 @@
-import { TRANSLATIONS, DEFAULT_LANGUAGE_CODE, type LoginTranslation } from "./translations";
-
-export function getTranslation(langCode: string): LoginTranslation {
-  return TRANSLATIONS[langCode] ?? TRANSLATIONS[DEFAULT_LANGUAGE_CODE];
-}
